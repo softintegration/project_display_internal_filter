a5197f3b5ab695efa07cf923142bf98552960213
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-10-05 09:31:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Display internal projects filter
+--------------------------------
+When installing timesheet module,by default,the internal projects are not displayed,and we can not display them because the filter in embedded in the action view directly,this module allow to enable or disable this filter.
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+ 'name': 'Display internal projects filter',
+ 'author': 'Soft-integration',
+ 'application': False,
+ 'installable': True,
+ 'auto_install': False,
+ 'qweb': [],
+ 'description': False,
+ 'images': [],
+ 'version': '1.0.1',
+ 'category': 'Project',
+ 'demo': [],
+ 'depends': ['hr_timesheet'],
+ 'data': [
+     'views/project_views.xml',
+    ],
+ 'license': 'LGPL-3',
+ }
